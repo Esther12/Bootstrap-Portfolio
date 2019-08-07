2ac99201c23d7445fec7e661f9f7834124fd71ef
Yating(Esther) Su <yaringsu@foxmail.com>
2019-08-06 23:01:19 -0400

    add new style to the newIndex.html and finished the portfolio and the project part

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
 var currentDate = moment();
 var upLoadDate;
 var duration;
-
 var projectDate = ["06/26/2019","07/04/2019","07/09/2019","07/15/2019","07/11/2019","07/16/2019","07/19/2019","07/19/2019"];
 
 $(document).ready(function(){
     for(let i = 0; i < 8 ; i++){
-        upLoadDate = projectDate[i];
-        duration = moment.duration(upLoadDate.diff(currentDate,"days"));
-        $(`#project[${i}]`).text(duration);
+        upLoadDate = moment(projectDate[i]);
+        console.log(upLoadDate);
+        console.log(currentDate);
+        duration = upLoadDate.diff(currentDate,"days") *(-1);
+
+        $(`#project${i+1}`).text(duration);
         console.log(duration);
     }
 })
